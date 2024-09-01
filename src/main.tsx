29403262bc5e53d5fb2e52ea824a3b3722e128a0
Percival Aceron <perci.aceron@gmail.com>
2024-09-01 14:58:09 +0800

    progress

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ChakraProvider, theme } from "@chakra-ui/react";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </StrictMode>
+);
---
+++ src/styles/theme.tsx
@@ -0,0 +1,22 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    green1: "#3C8067",
+    green2: "#165415",
+    green3: "#477D23",
+    green4: "#336D58",
+    green5: "#1FAC1B",
+    green6: "#245B1E",
+    yellow: "#F8AA02",
+    orange: "#D27C13",
+    red: "#F20303",
+    red2: "#C52A2A",
+    pink: "#F3027E",
+  },
+  html: {
+    fontSize: "16px",
+  },
+});
+
+export default theme;
